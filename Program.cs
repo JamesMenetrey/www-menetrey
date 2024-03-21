6e65d5ea64414b78ccb2487e0029ecde1619671d
Jämes Ménétrey <james@menetrey.org>
2024-03-21 22:50:12 +0100

    Prerender the website upon compilation

--- Program.cs
+++ Program.cs
@@ -2,12 +2,22 @@ using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using PersonalWebsite;
 using PersonalWebsite.Data;
+using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-builder.Services.AddSingleton<PublicationList>();
+ConfigureServices(builder.Services, builder.HostEnvironment.BaseAddress);
 
 await builder.Build().RunAsync();
+
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+    // Nuget package for handing some head tags, compatible with prerendering.
+    // The built-in feature for head tags management creates duplicate tags (see #33).
+    services.AddHeadElementHelper();
+    
+    services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(baseAddress) });
+    services.AddSingleton<PublicationList>();
+}
